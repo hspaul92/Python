2b8ae5e48c7c19a26d016dedb30a847b0a02cbe8
hipaul <hspaul92@gmail.com>
2020-04-30 20:01:41 +0530

    Sol:2  Using iteration and dictionary key asignment.

--- GeeksForGeeks/Tuple/ConvertListOfTupleToDict.py
+++ GeeksForGeeks/Tuple/ConvertListOfTupleToDict.py
@@ -1,8 +1,23 @@
 # Solution:1 Using List Comprehension
-def convertListOfTupleToDict(l):
-    d={x[0]:x[1] for x in l}
-    print("Result Dictionary",d)
+def convertListOfTupleToDict1(l):
+    d1 = {x[0]: x[1] for x in l}
+    print("Result Dictionary", d1)
 
-lis= [(2001,'Amazon'),(2002,'Google'),(2003,'Apple')]
-print("Original list of tuple:",lis)
-convertListOfTupleToDict(lis)
+# Solution:2 Using List Comprehension  and dict key assignment method
+def convertListOfTupleToDict2(l):
+    d2={}
+    for x in l:
+        d2[x[0]]=x[1]
+    print("Result Dictionary", d2)
+
+
+
+# Driver programme
+lis = [(2001, 'Amazon'), (2002, 'Google'), (2003, 'Apple')]
+print("Original list of tuple:", lis)
+
+# Using Solution
+convertListOfTupleToDict1(lis)
+convertListOfTupleToDict2(lis)
+
+
