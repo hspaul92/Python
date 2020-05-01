a1451ea4c2e84cec3978653aa8c280f59e25566b
hipaul <hspaul92@gmail.com>
2020-05-01 11:50:29 +0530

    Little beautification in case part.

--- GeeksForGeeks/Tuple/ZipUnevenTuple.py
+++ GeeksForGeeks/Tuple/ZipUnevenTuple.py
@@ -1,17 +1,20 @@
 # Case:1 If Corresponding element is available then zip it ,
 #             If not, then Zip with Null
-
 # Example:
 # First Input  : (2001, 2002, 2003, 2004, 2005, 2006)
 # Second Input : ('AWS', 'GOOGLE', 'APPLE')
 # FirstZipSecond :((2001, 'AWS'), (2002, 'GOOGLE'), (2003, 'APPLE'), (2004, 'None'), (2005, 'None'), (2006, 'None'))
 # SecondZipFirst :(('AWS', 2001), ('GOOGLE', 2002), ('APPLE', 2003), ('None', 2004), ('None', 2005), ('None', 2006))
 
+# **************************************************************
+
+
 # Case:2 If Corresponding element is available then zip it ,
 #             If not, use cyclic zip
 
 
 
+# *************************************************************
 # For Case:1
 # Solution:1 Using list() and append()
 def  zipUnevenTuple1(t1,t2):
